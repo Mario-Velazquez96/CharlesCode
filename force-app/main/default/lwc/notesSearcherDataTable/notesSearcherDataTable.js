30774341a2f676996bb3db2dc66c5d48bbbf4255
Mario-Velazquez96 <mario.velazquez@yeti.com>
2023-07-20 14:37:58 -0600

    changing date

--- force-app/main/default/lwc/notesSearcherDataTable/notesSearcherDataTable.js
+++ force-app/main/default/lwc/notesSearcherDataTable/notesSearcherDataTable.js
@@ -145,7 +145,7 @@ export default class NotesSearcherDataTable extends LightningElement {
         const month = String(date.getMonth() + 1).padStart(2, '0'); // January is 0!
         const year = date.getFullYear();
 
-        return day + '/' + month + '/' + year;
+        return month + '/' + day + '/' + year;
     }
 
 }
