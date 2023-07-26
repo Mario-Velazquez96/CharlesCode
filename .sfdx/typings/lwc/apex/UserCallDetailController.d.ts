1e373e8967bdaaedc6e9dea167549284593fe3ee
Mario-Velazquez96 <mario.velazquez@yeti.com>
2023-07-26 08:24:06 -0600

    idk changes

--- .sfdx/typings/lwc/apex/UserCallDetailController.d.ts
+++ .sfdx/typings/lwc/apex/UserCallDetailController.d.ts
@@ -1,3 +1,3 @@
 declare module "@salesforce/apex/UserCallDetailController.getUserCallDetails" {
-  export default function getUserCallDetails(param: {userId: any}): Promise<any>;
+  export default function getUserCallDetails(param: {userId: any, startDate: any, endDate: any}): Promise<any>;
 }
